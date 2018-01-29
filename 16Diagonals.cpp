258ad344901210edfb144ee2b0753df4b5fc1b97
William Miao <wimiao@microsoft.com>
2018-01-29 09:46:12 -0800

    fixed bugs

--- 16Diagonals.cpp
+++ 16Diagonals.cpp
@@ -18,7 +18,7 @@ void checkPos(const vector<vector<int>> &x, int &i, int &j) {
 
 void checkBackwards(const vector<vector<int>> &x, int &i, int &j) {
     if (j == 0) {
-        if (i == 0) return;
+        if (i == 0) return; // first row
         // otherwise, we need to move to the previous row;
         i--;
         j = x[i].size() - 1;
@@ -37,17 +37,17 @@ bool can_be_extended(const vector<vector<int>> &x, int &i, int &j) {
         if(j > 0 && x[i][j-1] == 2) return false;
         if(j < x[i].size() - 1 && x[i][j+1] == 2) return false;
         // check 1
-        if(i > 0 && j < x[i].size() - 1 && x[i-1][j+1] == 1) return false;
-        if(i < x.size() - 1 && j > 0 && x[i+1][j-1] == 1) return false;
+        if(i > 0 && j > 0 && x[i-1][j-1] == 1) return false;
+        if(i < x.size() - 1 && j < x[i].size() - 1 && x[i+1][j+1] == 1) return false;
     } else if(x[i][j] == 2){
         // check 1
         if(i > 0 && x[i-1][j] == 1) return false;
-        if(i < x.size() - 1 && x[i+1][j] == 1)
+        if(i < x.size() - 1 && x[i+1][j] == 1) return false;
         if(j > 0 && x[i][j-1] == 1) return false;
         if(j < x[i].size() - 1 && x[i][j+1] == 1) return false;
         // check 2
-        if(i > 0 && j > 0 && x[i-1][j-1] == 2) return false;
-        if(i < x.size() - 1 && j < x[i].size() - 1 && x[i+1][j+1] == 2) return false;
+        if(i > 0 && j < x[i].size() - 1 && x[i-1][j+1] == 2) return false;
+        if(i < x.size() - 1 && j > 0 && x[i+1][j-1] == 2) return false;
     }
     /* cout << "INSIDE CAN BE EXteNDED RIGHT BEFORE CHECKPOS: " << i << " " << j << '\n'; */
     checkPos(x, i, j);
@@ -79,27 +79,15 @@ void extend(vector<vector<int>> &x, int n, int count, int i, int j) {
         x[i][j] = z;
         /* cout << "Z: " << z << '\n'; */
         if(can_be_extended(x, i, j)) {
-            if(count >= 13) {
-                for(int i=0; i < x.size(); i++) {
-                    for(int j=0; j < x[i].size(); j++) {
-                        cout << x[i][j] << ' ';
-                    }
-                    cout << '\n';
-                }
-                cout << "COUNT: " << count << '\n';
-            }
-            /* if(count++ == n) { */
-            /*     cout << "DONE" << '\n'; */
+            /* if(count >= 13) { */
             /*     for(int i=0; i < x.size(); i++) { */
             /*         for(int j=0; j < x[i].size(); j++) { */
             /*             cout << x[i][j] << ' '; */
             /*         } */
             /*         cout << '\n'; */
             /*     } */
-            /*     exit(0); */
+            /*     cout << "COUNT: " << count << '\n'; */
             /* } */
-            /* count--; */
-            /* cout << "COUNT: " << count << '\n'; */
             if(i == x.size() - 1 && j == x[i].size() - 1 && count != n - 1) {
                 x[i][j] = 3;
                 return;
@@ -112,7 +100,8 @@ void extend(vector<vector<int>> &x, int n, int count, int i, int j) {
         }
     }
 
-    /* checkBackwards(x, i, j); */
+    checkBackwards(x, i, j);
+    /* extend(x, n, count, i, j); */
     /* x[i][j] = -1; */
     return;
 
@@ -148,7 +137,7 @@ int main() {
         {-1,-1,-1,-1,-1}
     };
     int count = 0;
-    extend(x, 15, count, 0, 0);
+    extend(x, 16, count, 0, 0);
         /* for(int i=0; i < x.size(); i++) { */
         /*     for(int j=0; j < x[i].size(); j++) { */
         /*         cout << x[i][j] << ' '; */
